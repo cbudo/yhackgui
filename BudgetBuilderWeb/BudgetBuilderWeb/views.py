7d3d25436b8a5cc41bcdb8bbc0c8a168b3af3e57
cbudo <budocf@rose-hulman.edu>
2015-11-08 07:59:43 -0500

    initial

---
+++ BudgetBuilderWeb/BudgetBuilderWeb/views.py
@@ -0,0 +1,46 @@
+﻿"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from BudgetBuilderWeb import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/location/<company>')
+def location(company):
+    return render_template(
+        'location.html',
+        company=company,
+        title='Location',
+        year=datetime.now().year
+    )
